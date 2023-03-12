3ccfba23627544d5fe947869fe1e2fc7bb4bc40a
Pavel Spichakov <spv-pavel@yandex.ru>
2023-03-12 13:48:45 +0300

    create depository

---
+++ app.py
@@ -0,0 +1,44 @@
+import telebot
+from config import keys, TOKEN
+from utils import ConversionException, CryptoConverter
+
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def help_(message: telebot.types.Message):
+    text = 'Чтобы начать работу введите команду боту в следующем формате:\n<имя валюты> \
+<в какую валюту перевести> \
+<количество переводимой валюты> \nУвидеть список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values_ = message.text.split(' ')
+
+        if len(values_) != 3:
+            raise ConversionException('Не верное количество параметров!')
+
+        quote, base, amount = values_
+        total_base = CryptoConverter.convert(quote, base, amount)
+    except ConversionException as e:
+        bot.reply_to(message, f'Ошибка пользователя\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+    else:
+        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        bot.send_message(message.chat.id, text)
+
+
+bot.polling()
